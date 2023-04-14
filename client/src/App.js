44a02ec03a1aceb1d41d9574ede8fb530075c2c7
Facundovila03 <facundovila03@gmail.com>
2023-04-13 23:56:55 -0300

    LandingPage done, added design folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/Landing Page/LandingPage";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <script>console.log('hola')</script>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
--- client/src/components/Landing Page/LandingPage.jsx
+++ client/src/components/Landing Page/LandingPage.jsx
@@ -1,5 +1,26 @@
-import styles from "./LandingPage.modules.css";
+import styles from "./LandingPage.module.css";
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
-  return ( <div className={styles.Contenedor}>dd</div>)
+  return (
+    <div className={styles.Contenedor}>
+      <div className={styles.DivLargo}>WELCOME</div>
+      <div className={styles.Welcome}>
+        <div style={{ width: "30vw" }}>
+          <h2>Muchas gracias por visitar mi pagina...</h2>
+          <h4>
+            Esta fue desarrollada por mi, Facundo Vila, con el fin de ser
+            presentada como proyecto individual para el bootcamp de Soy henry
+            del que formo parte. Dandole a next vas a poder adentrate en la
+            parte de "home", donde podes buscar y crear recetas entre otros!! te
+            espero del otro lado :)
+          </h4>
+          <Link to={"/home"} className={styles.Boton}>
+            Next
+            {/* <button className={styles.Boton}>Next</button> */}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
